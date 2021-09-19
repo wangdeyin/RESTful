0eb77acabb32d69978c7496dc13062defc9d6668
wangdy <wang_dyin@163.com>
2021-09-19 18:06:03 +0800

    1

---
+++ src/main/java/com/example/restservice/controller/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.restservice.controller;
+
+import com.example.restservice.entity.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
