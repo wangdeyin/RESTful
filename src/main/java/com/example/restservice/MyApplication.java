e2541d9969bdc9fc0192c54dd7b8a483200e24f1
wangdy <wang_dyin@163.com>
2021-09-20 10:05:01 +0800

    定时任务

--- src/main/java/com/example/restservice/MyApplication.java
+++ src/main/java/com/example/restservice/MyApplication.java
@@ -2,6 +2,7 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
---
+++ src/main/java/com/example/restservice/controller/ScheduledTasks.java
@@ -0,0 +1,24 @@
+package com.example.restservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@EnableScheduling
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("现在的时间是{}", dateFormat.format(new Date()));
+    }
+}
